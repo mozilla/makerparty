48c40ee813a5a2236670b9db16239fd48fee3219
Mavis Ou <mmmavisss@gmail.com>
2014-03-31 17:36:27 -0700

    Update site header; initial attempt to use nunjucks

--- server/config.js
+++ server/config.js
@@ -1,22 +1,39 @@
 module.exports = function (env) {
   var express = require('express');
+  var nunjucks = require( "nunjucks" );
   var path = require('path');
+  var i18n = require( "../lib/i18n.js" );
+  console.log(i18n);
   var app = express();
+  var nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( "./public"));
   var defaultLang = 'en-US';
+  var appVersion = require('../package').version;
 
   app.use(express.logger('dev'));
   app.use(express.compress());
   app.use(express.json());
   app.use(express.urlencoded());
 
+  nunjucksEnv.express( app );
+
   var config = {
-    version: require('../package').version
+    version: appVersion
   };
 
   var healthcheck = {
-    version: require('../package').version,
+    version: appVersion,
     http: 'okay'
   };
+
+  // Setup locales with i18n
+  // app.use( i18n.abide({
+  //   supported_languages: [
+  //     "en_US"
+  //   ],
+  //   default_lang: "en_US",
+  //   translation_directory: "locale",
+  //   localeOnUrl: true
+  // }));
 
   // Static files
   app.use(express.static('./public'));
@@ -31,6 +48,11 @@ module.exports = function (env) {
     res.setHeader('Content-type', 'text/javascript');
     res.send('window.eventsConfig = ' + JSON.stringify(config));
   });
+
+  app.get('/hello', function(req, res){
+    res.render( "index.html", { helloworld: "Hello World"});
+  });
+
 
   return app;
 };
