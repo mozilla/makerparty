a4b1f2506c42eb7fc0bd9d1bf3b6a7fe0dc6ce23
Mavis Ou <mmmavisss@gmail.com>
2014-04-14 18:29:18 -0700

    Styling tweaks & add in the 2nd testimonial quote

--- public/js/ui.js
+++ public/js/ui.js
@@ -2,3 +2,22 @@ $(".collage-pics").colorbox({
                       rel: "collage-pics"
                    });
 
+
+// Auto rotates quotes
+
+function rotate(elemClass) {
+  var current = $("." + elemClass + ".current");
+  current.delay(10000).fadeOut(1000,function(){
+    $(this).toggleClass("current");
+    var nextQuote = ( $(this).next("."+elemClass).length > 0 ) ?
+                          $(this).next("."+elemClass) : $(this).siblings("."+elemClass).eq(0);
+    nextQuote.fadeIn(1000).toggleClass("current");
+  });
+}
+
+function autoRotatingQuotes(elemClass){
+  rotate("quote-text");
+  rotate("quote-source");
+}
+
+setInterval(autoRotatingQuotes, 13000);
