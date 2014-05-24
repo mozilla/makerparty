44bc25785e4836362579af7bface4458188014b6
William Duyck <fuzzyfox0@gmail.com>
2014-05-24 02:20:03 +0100

    initial pass at generating needed stats for MP heatmap

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var nunjucks = require('nunjucks');
 var path = require('path');
 var i18n = require('webmaker-i18n');
+var request = require('request');
+var NodeCache = require('node-cache')
+var cache = new NodeCache({stdTTL: 300, checkperiod: 300});
 var nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader(path.join(__dirname, 'views')));
 
 habitat.load();
@@ -64,4 +67,111 @@ app.get('/strings/:lang?', i18n.stringsRoute('en-US'));
 // Run server
 app.listen(env.get('PORT'), function () {
   console.log('Now listening on %d', env.get('PORT'));
+});
+
+// Maker Party Event Stats
+app.get('/event-stats', function(req, res) {
+  var data = cache.get('eventStats');
+  res.json( data.eventStats );
+});
+
+function generateEventStats() {
+  request.get(env.get('EVENTS_SERVICE') + '/events?after=' + env.get('EVENTS_START_DATE'), function(error, response, events) {
+    if(!error && response.statusCode === 200) {
+      events = JSON.parse( events );
+      // arrays to dedupe things w/
+      var event_hosts = [];
+      var countries = [];
+
+      // init stats object
+      var event_stats = {
+        hosts: 0,
+        attendees: 0,
+        events: 0,
+        byCountry: {}
+      };
+
+      // aleady know number of events :)
+      event_stats.events = events.length;
+      var geocodes = events.length;
+
+      // make access to other stats easier to figure
+      events.forEach( function( event, idx ) {
+        var new_host = 0; // little trick to count hosts per country
+
+        /*
+          get people stats
+         */
+
+        // get attendee stats
+        event_stats.attendees += event.attendees;
+
+        // get host stats
+        if(event_hosts.indexOf(event.organizerId) === -1){
+          event_hosts.push(event.organizerId);
+
+          new_host = 1; // this is a new host, add to country host count ;)
+        }
+
+        /*
+          get country level stats
+         */
+        if( event.country === null ) {
+          event.country = 'UNKNOWN';
+        }
+
+        if( countries.indexOf( event.country ) === -1 ) {
+          event_stats.byCountry[ event.country ] = {
+            name: event.country,
+            events: 0,
+            hosts: 0,
+            attendees: 0
+          };
+          countries.push( event.country );
+        }
+
+        var country = event_stats.byCountry[ event.country ];
+
+        country = {
+          name: country.name,
+          events: country.events + 1,
+          hosts: country.hosts + new_host,
+          attendees: country.attendees + event.attendees
+        };
+
+        event_stats.byCountry[ event.country ] = country;
+      });
+
+      // now we can count number organizers  (deduped)
+      event_stats.hosts = event_hosts.length;
+
+      // sort the country based data using keys
+      var country_keys = Object.keys(event_stats.byCountry);
+      var new_byCountry = {};
+
+      country_keys.sort();
+
+      for ( var i = 0; i < country_keys.length; i++ ) {
+        var key = country_keys[ i ];
+        new_byCountry[ country_keys[ i ] ] = event_stats.byCountry[ key ];
+      }
+
+      event_stats.byCountry = new_byCountry;
+
+      cache.set('eventStats', event_stats);
+    }
+    else if(error) {
+      cache.set('eventStats', { processing: true });
+    }
+  });
+}
+cache.set('eventStats', { processing: true });
+generateEventStats();
+
+
+cache.on( 'expired', function( key, value ) {
+  if( key === 'eventStats' ) {
+    cache.set('eventStats', value);
+    generateEventStats();
+  }
 });
